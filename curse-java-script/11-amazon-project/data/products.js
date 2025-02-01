2a1fe9f77d402f9313aa3cd1c7231e1064d711c2
leandrogalbarino <leandrokise753@gmail.com>
2025-01-31 21:25:45 -0300

    Removendo codigo de teste

--- curse-java-script/11-amazon-project/data/products.js
+++ curse-java-script/11-amazon-project/data/products.js
@@ -22,7 +22,7 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
-  
+
   // Method Overrriding 
   extraInfoHTML() {
     return '';
@@ -31,6 +31,7 @@ class Product {
 
 // Inheritance
 // Reuse code between classes
+// All methods of Product contain in Cloating class, because Cloating is extend of Product
 class Clothing extends Product {
   sizeChartLink;
 
@@ -52,27 +53,6 @@ class Clothing extends Product {
     `;
   }
 }
-
-const tshirt = new Clothing({
-  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799,
-  keywords: [
-    "tshirts",
-    "apparel",
-    "mens"
-  ],
-  type: "clothing",
-  sizeChartLink: "images/clothing-size-chart.png"
-});
-// All methods of Product contain in Cloating class, because Cloating is extend of Product
-
-
 
 export const products = [
   {
