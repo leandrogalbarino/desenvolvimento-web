af8fa8a04b1d9ccaf879dd62fbbed98c463904f2
leandrogalbarino <leandrokise753@gmail.com>
2025-02-19 19:15:02 -0300

    Estilos e classes

--- curse-java-script/12-2.0-dom/11-working-with-styles/script.js
+++ curse-java-script/12-2.0-dom/11-working-with-styles/script.js
@@ -3,6 +3,21 @@ const itemList = document.querySelector('.item-list');
 const items = document.querySelectorAll('li');
 
 document.querySelector('button').addEventListener('click', () => {
-  console.log(itemList.className);
-  text.className = 'card dark';
+  // console.log(itemList.className);
+  // text.className = 'card dark';
+
+  // List classes
+  // console.log(itemList.classList);
+  // See classes 
+  // itemList.classList.forEach((clas) => console.log(clas));
+  
+  // text.classList.add('dark');
+  // text.classList.remove('card');
+  
+  // text.classList.toggle('hidden');
+  text.classList.replace('card', 'dark');
+
+  // Change
+  
+  text.classList.forEach((clas) => console.log(clas));
 });
