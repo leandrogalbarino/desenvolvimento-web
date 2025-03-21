44d9a6bc7525875b7ba07213fc2c373736e048d2
leandrogalbarino <leandrokise753@gmail.com>
2025-03-21 16:23:14 -0300

    Rotas Dinâmicas.

--- react/movies/src/pages/Watch/index.js
+++ react/movies/src/pages/Watch/index.js
@@ -2,14 +2,29 @@ import styles from "./Watch.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
+import videos from "../../json/videos.json";
+import { useParams } from "react-router-dom";
+import PageNotFound from "../PageNotFound";
 
 function Watch() {
+  
+  const params = useParams();
+  const video = videos.find((video) => {
+    return video.id === params.id;
+  });
+  if(!video){ return <PageNotFound />}
+
   return (
     <>
       <Header />
       <Container>
         <section className={styles.assistir}>
-          <iframe width="854" height="480x" src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=ll8JjsIKlnINXlzw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe
+            width="854" height="480x"
+            src={`https://www.youtube.com/embed/${video.id}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
       </Container>
       <Footer />
--- react/movies/src/routes.js
+++ react/movies/src/routes.js
@@ -9,7 +9,7 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/watch" element={<Watch />} />
+        <Route path="/watch/:id" element={<Watch />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>    
