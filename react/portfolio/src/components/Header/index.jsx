18cd981ddb6912f5c8792cfb539d171e7a3bfb67
leandrogalbarino <leandrokise753@gmail.com>
2026-01-06 11:15:59 -0300

    Mandando mais conteúdo.

---
+++ react/portfolio/src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import styles from './Header.module.css';
+// import 
+function Header() {
+  
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <span>leandroGalbarino.dev</span>
+        <nav>
+          <a href="$"></a>
+          <a href="$"></a>
+          <a href="$"></a>
+          {/* <Link to="#"></Link>
+          <Link to="#"></Link>
+          <Link to="#"></Link> */}
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+
+
+export default Header;
